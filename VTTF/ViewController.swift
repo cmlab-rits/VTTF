5f39bda69882b9401b377ee3eeacdfbaec09b6a1
Yuki TAKEDA <teakun@kittengine.com>
2017-09-08 14:54:52 +0900

    Initial Commit

---
+++ VTTF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VTTF
+//
+//  Created by Yuki Takeda on 2017/09/08.
+//  Copyright © 2017年 cm.is.ritsumei.ac.jp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
